4b4b36902d0daf5d10288d1bfcd334093aeebd05
Larry Arrington <larryarrington@pursuit.org>
2023-03-22 10:30:49 -0400

    add comments to walkthrough understanding

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let dropdown = ce({ tagname: "select", id: "dropdown" });
+let dropdown = ce({ tagname: "select", id: "dropdown" }); // create a dropdown element `ce` is a function that creates an element
 dropdown.append(
   ...[
     { tagname: "option", value: "", innerText: "Random" },
@@ -8,7 +8,7 @@ dropdown.append(
   ].map((el) => ce(el))
 );
 
-document.querySelector("form").prepend(dropdown);
+document.querySelector("form").prepend(dropdown); // prepend the dropdown to the form. prepend is a method that adds an element to the beginning of a parent element
 document.querySelector("form").addEventListener("submit", (evt) => {
   evt.preventDefault();
   getResponse(document.querySelector("main"), evt.target.dropdown.value);
@@ -25,7 +25,7 @@ async function getResponse(main, level = "medium") {
   } catch (error) {
     error_handling(error);
   }
-}
+} // getResponse is an async function that fetches data from the api and creates a card for each question. aync functions return a promise and can be awaited.
 
 function create_card(el) {
   let answer = ce({ style: "margin:15px 3px;" });
@@ -40,7 +40,7 @@ function create_card(el) {
       let false_ = ce({ tagname: "label", innerText: "False" });
       false_.append(
         ce({ tagname: "input", type: "radio", name: id, value: false })
-      );
+      ); // create a label element for true and false and append an input element to each label. append the labels to the answer element. append is used to add an element to the end of a parent element. In this case the parent element is the answer element
 
       correct_answer = el.correct_answer ? true_ : false_;
       answer.append(true_, false_);
@@ -61,7 +61,7 @@ function create_card(el) {
         style: "display:Block;margin:15px 3px;",
         innerHTML: el.correct_answer,
       });
-      acc.push(correct_answer);
+      acc.push(correct_answer); // create a label element for each incorrect answer and the correct answer. append the labels to the answer element
 
       for (let x of acc)
         x.prepend(ce({ tagname: "input", type: "radio", name: id }));
@@ -69,7 +69,7 @@ function create_card(el) {
       break;
     default:
       error_handling("unknown question type");
-  }
+  } // switch statement that creates a label element for each answer and appends an input element to each label. append is used to add an element to the end of a parent element. In this case the parent element is the answer element
 
   let article = ce({ tagname: "article", class: `card ${el.difficulty}` });
   article.append(
@@ -84,7 +84,7 @@ function create_card(el) {
     ce({ tagname: "p", class: "hidden", innerText: el.correct_answer })
   );
   return article;
-}
+} // create_card is a function that creates a card element for each question. the card element is appended to the main element
 
 function ce(obj) {
   let rst = document.createElement(obj.tagname || "div");
@@ -103,19 +103,18 @@ function ce(obj) {
         rst.setAttribute(x, obj[x]);
     }
   return rst;
-}
+} // the ce function creates an element. the obj argument is an object that contains the attributes of the element. the obj argument can also contain an event_ attribute that contains an object with event listeners. the event_ attribute is used to add event listeners to the element
 
 function show_correct_answer(evt) {
   setTimeout(() => this.classList.remove("correct"), 2000);
   this.classList.add("correct");
-}
+} // show_correct_answer is a function that adds a class to the correct answer
 
 function error_handling(text) {
   document.querySelector("main").innerHTML = `<p class='error'>${text}</p>`;
-}
+} // error_handling is a function that displays an error message
 
 function shuffle(array) {
-  //from stack overflow
   let currentIndex = array.length,
     randomIndex;
   while (currentIndex != 0) {
@@ -127,4 +126,4 @@ function shuffle(array) {
     ];
   }
   return array;
-}
+} // shuffle is a function that shuffles an array thank you stack overflow.
