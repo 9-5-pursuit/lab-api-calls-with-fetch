709457dedff2a87d816061b3672f9b2cf5db3699
davep80 <davidpaq1@gmail.com>
2023-03-21 23:58:09 -0400

    added category select feature

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 const BASE_URL = "https://opentdb.com/api.php?amount=10";
 
+const form = document.querySelector('form');
+const select = document.createElement('select');
+select.setAttribute('name', 'category');
+select.setAttribute('id', 'category');
+select.style.margin = '5px'
+let option1 = document.createElement('option');
+select.setAttribute('value', "any")
+option1.textContent = "--Any Category--";
+select.appendChild(option1);
+
+fetch("https://opentdb.com/api_category.php").then(response => response.json()).then(
+    items => {
+        items['trivia_categories'].forEach(item => {
+            let option1 = document.createElement('option');
+            option1.setAttribute('value', item['id'])
+            option1.textContent = item['name'];
+            select.appendChild(option1);
+        })
+    }
+).catch(e => console.log(e))
+// Insert the select element before the button element in the form
+const button = form.querySelector('button');
+form.insertBefore(select, button);
+
 document.querySelector('form').addEventListener("submit", (event) => {
     event.preventDefault()
 
-    fetch(BASE_URL).then((response) => response.json())
-        .then(displayCard => {
+    const category = form.elements.category.value;
+    let addon;
+    if (category == '--Any Category--') addon = ''
+    else addon = "&category=" + category
 
+    fetch(`${BASE_URL}${addon}`).then((response) => response.json())
+        .then(displayCard => {
             const mainElement = document.querySelector('.centered');
+            const articleElements = mainElement.querySelectorAll('article');
+            for (let i = 0; i < articleElements.length; i++) {
+                mainElement.removeChild(articleElements[i]);
+            }
             displayCard['results'].forEach(item => {
                 const articleElement = document.createElement('article');
                 articleElement.setAttribute('class', 'card');
-
                 articleElement.innerHTML = `
                 <h2>${item.category}</h2>
                 <p>${item.question}</p>
@@ -29,6 +60,5 @@ document.querySelector('form').addEventListener("submit", (event) => {
                 mainElement.appendChild(articleElement);
             });
         }
-        )
-        .catch(displayError => console.log('no'));
+        ).catch(displayError => console.log(displayError));
 })
