8c634428a8c7425acc0c639b31b243f1d195d6f0
ambarimar ulloa <ambarimar@ambarimars-Air.fios-router.home>
2023-03-22 00:38:55 -0400

    done

---
+++ app.js
@@ -0,0 +1,46 @@
+
+const formOne = document.querySelector("form button").addEventListener("submit", (event) => {
+    event.preventDefault();
+});
+
+
+fetch("https://opentdb.com/api.php?amount=10")
+
+ .then((response) => {
+    
+    return response.json();
+})
+.then((output) => {
+    console.log(output.results)
+
+     const main = document.querySelector("main");
+     let array = output.results;
+
+    array.forEach((element) => {
+
+         const article = document.createElement("article");
+         article.classList.add("card");
+          article.innerHTML = `<h2>${element.category}</h2><p>${element.question}</p><button class='answer'>Show Answer</button><p class='hidden'>${element.correct_answer}</p>`;
+          main.append(article);
+
+         const button = document.querySelector(".answer");
+        
+         const hiddenclass = document.querySelector(".hidden");
+
+         button.addEventListener("click", (event) => {
+            hiddenclass.style.display = "block";
+        })
+
+     })
+
+})
+.catch((error) => {
+    console.log(error);
+
+});
+
+
+
+
+
+
