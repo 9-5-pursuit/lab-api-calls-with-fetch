37afec8ba69d0f5326cbd218ecf4f12142e19a5e
charellpeterkin <119958341+charellpeterkin@users.noreply.github.com>
2023-03-22 11:32:36 -0400

    with class help

--- app.js
+++ app.js
@@ -1,17 +1,68 @@
-// https://opentdb.com/api.php?amount=10
+let api_url = "https://opentdb.com/api.php?amount=10"
+let form = document.querySelector("form")
+let main = document.querySelector("main")
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-fetch("https://opentdb.com/api.php?amount=10").then(res => {
-    return res.json()
+    fetch(api_url)
+    .then((response) => response.json())
+    .then((json) => {
+        const results = json.results
+        results.forEach(result => {
+            appResult(result);
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 })
- .then(triviaQuestions => {
-  triviaQuestions.forEach(element => {
-    console.log(element)
-  });
- })
- .catch(error => {
-    console.log("error")
- } )
+
+const appResult = (result) => {
+    const card = document.createElement("article");
+    card.classList.add("card")
+
+    const category = document.createElement("h2")
+    category.textContent = result.category
+
+    const question = document.createElement("p")
+    question.innerText = result.question
+
+    const button = document.createElement("button")
+    button.innerText = "Show Answer"
+
+
+    const p2 = document.createElement("p")
+    p2.classList.add("hidden")
+    p2.innerHTML = result.correct_answer;
+
+    button.addEventListener("click", () => {
+       p2.classList.toggle(`hidden`);
+
+       if (p2.classList.contains(`hidden`)) {
+        p2.classList.remove("hidden")
+    } else {
+        p2.classList.add("hidden")
+    }
+    })
+
+    card.append(category, question, button, p2);
+    main.append(card);
+
+}
+
+
+// fetch("https://opentdb.com/api.php?amount=10").then(res => {
+//     return res.json()
+// })
+//  .then(triviaQuestions => {
+//   triviaQuestions.forEach(element => {
+//     console.log(element)
+//   });
+//  })
+//  .catch(error => {
+//     console.log("error")
+//  } )
 
 
 // async function triviaApp() {
