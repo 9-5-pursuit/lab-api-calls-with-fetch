f1cab4747dc1ef1298bbec9956cc99833867f0de
Mussie <mussierussom@pursuit.org>
2023-04-07 16:16:50 -0400

    Correction is done for api-calls-with-fetch

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// const trivia_API = "https://opentdb.com/api.php?amount=10";
-// fetch(trivia_API)
-//   .then((response) => response.json())
-//   .then((json) => {
-//     console.log(json);
-//     parsedData(json);
-//     console.log("----------------++++++");
-//   })
-//   .catch((error) => {
-//     console.log("Error");
-//   });
-
-// function parsedData(data) {
-//   const mainA = document.querySelector(".card");
-//   const form = document.querySelector("form");
-//   //   form.addEventListener("submit", (e) => {
-//   //     e.preventDefault();
-
-//   console.log("----------------");
-//   data.results.forEach((element) => {
-//     const question = element.question;
-//     const answer = element.correct_answer;
-//     console.log(`Question: ${question} ,   =>> Answer: ${answer}`);
-//   });
-//   console.log("----------------");
-//   const quesTag = document.createElement("p");
-//   quesTag.textContent = question;
-//   //   });
-// }
-
 const trivia_API = "https://opentdb.com/api.php?amount=10";
 const main = document.querySelector("main");
 const form = document.querySelector("form");
@@ -51,8 +21,6 @@ form.addEventListener("submit", (e) => {
 });
 
 const displayResult = (result) => {
-  console.log(result);
-
   const card = document.createElement("article");
   card.classList.add("card");
 
@@ -69,25 +37,9 @@ const displayResult = (result) => {
   p2.classList.add("hidden");
   p2.innerHTML = result.correct_answer;
 
-  // card.append(category, p1, button, p2);
-  // main.append(card);
-
   button.addEventListener("click", () => {
     p2.classList.toggle("hidden");
   });
-
   card.append(category, p1, button, p2);
   main.append(card);
 };
-/*
-if (p2.classList.contains('hidden')) {
-    p2.classList.remove("hidden");
-}
-else {
-    p2.classList.add("hidden");
-}
-} or you use toggle
-*/
-
-// card.append(category, p1, button, p2);
-// main.append(card);
